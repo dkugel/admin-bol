0b12dcde19206f82308fb23798114eade5529246
Diego Herrera <diego.herrera@velezcaicedo.com>
2019-03-29 17:56:55 -0500

    Update hello.blade.php

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -49,6 +49,36 @@
                     <?php echo $data->links(); ?>                
                 </div>                                    
             </div>
+
+            <div class="container">
+                    <h2>Modal Example</h2>
+                    <!-- Trigger the modal with a button -->
+                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+                  
+                    <!-- Modal -->
+                    <div class="modal fade" id="myModal" role="dialog">
+                      <div class="modal-dialog">
+                      
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                          <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            <h4 class="modal-title">Modal Header</h4>
+                          </div>
+                          <div class="modal-body">
+                            <p>Some text in the modal.</p>
+                          </div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                          </div>
+                        </div>
+                        
+                      </div>
+                    </div>
+                    
+                  </div>
+
+
             <div class="row">
                 <div class="col-md-2 col-md-offset-10">
                     <img src="/images/velez-caicedo.svg" style="max-width: 150px;">
@@ -86,5 +116,6 @@
             </div>
         </div>
     </footer>
-	
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 @endsection
